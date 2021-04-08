b99843b970c30c75c912e4ed5b8b3c81fd6009d1
Chris Brown <chris.brown@sage.com>
2021-04-08 11:57:11 -0700

    defined IMessageService

--- src/Services/IMenuService.cs
+++ src/Services/IMenuService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace KitchenApi
@@ -5,5 +6,15 @@ namespace KitchenApi
     public interface IMenuService
     {
         ICollection<MenuItem> GetMenu();
+        
+        ICollection<MenuItem> GetMenu(MenuType type);
+
+        ICollection<MenuItem> GetMenu(DateTime time);
+
+        MenuItem GetMenuItem(int id);
+
+        MenuItem UpsertMenuItem(MenuItem name);
+
+        void DeleteMenuItem(int id);
     }
 }
